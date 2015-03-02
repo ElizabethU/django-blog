cba4eb0f601a4e29cee2260b1588ad76bc7734fb
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2015-03-01 21:59:40 -0800

    Added admin classes

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from myblog.models import Post, Category
 
+# class CategoryInline(admin.TabularInline):
+#     model = Post.categorys.through
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  # inlines = [
+  #   CategoryInline
+  # ]
+  pass
+
+class CategoryAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
